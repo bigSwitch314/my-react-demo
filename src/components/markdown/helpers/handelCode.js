6cb5778b6b05c314e52cb574bc126b87ba208f8c
luoqiang <luoqiang314@gmail.com>
2019-05-06 21:03:05 +0800

    markdown code 序号样式修改

--- src/components/markdown/helpers/handelCode.js
+++ src/components/markdown/helpers/handelCode.js
@@ -3,20 +3,24 @@ function handleCode(str) {
 
   str = str.replace(/\n/g, '!@#')
   let newStr = ''
-  newStr = str.replace(/<code>(.*?)<\/code>/g, (v, $1) => {
+  newStr = str.replace(/<code(.*?)>(.+?)<\/code>/g, (v, $1, $2) => {
     const code = [];
     const gutter = [];
-    const arr = ($1).split('!@#')
+    const arr = ($2).split('!@#')
     arr.forEach((item, index)=> {
       code.push('<div class="line">' + '&nbsp;' + item + '</div>');
       gutter.push('<div class="line">' + ++index + '</div>');
     });
-    
+  
     return  '<code>' +
-              '<table style="background:#f7f7f7;margin-bottom:8px;display:inline-flex;width:100%;overflow:auto">' + 
-                  '<tr style="background-color: #eff2f3;border: none">'+
-                    '<td style="border:none;">' + gutter.join('')+ '</td>'+
-                    '<td style="border: none">' + code.join('')+ '</td>'+
+              '<table style="background:#f7f7f7;width:100%">' +
+                  '<tr style="border: none">'+
+                    '<td style="border:none; background-color: #eff2f3; text-align: center; width: 24px">' +
+                      gutter.join('') +
+                    '</td>'+
+                    '<td style="border: none; background-color: #f8f8f8;">' +
+                      code.join('') +
+                    '</td>'+
                   '</tr>'+
               '</table>'+
             '</code>'
