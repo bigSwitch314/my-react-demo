d79ebd31a4ff1feac7eacba785d2998021a80e73
luoqiang <luoqiang314@gmail.com>
2019-06-21 18:07:37 +0800

    eslint.js文件修改

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,7 +17,9 @@ module.exports = {
   "rules": {
     "react/prop-types": "on",
     "no-console": "off",
-    "react/display-name": 0
+    "react/display-name": 0,
+    "no-unused-vars": 1,  // 未使用变量检测开启，warning级别
+    "no-undef": 2,  // 未定义变量检测开启，error级别
   },
   "env": {
     "es6": true,
